5b0be7877fd652c1688b56fb44293caf6ab16235
brunocoronado49 <francorangelcoronado@gmail.com>
2023-09-08 10:38:17 -0600

    tuplas

--- 03-dia/tuplas.py
+++ 03-dia/tuplas.py
@@ -1,3 +1,13 @@
 mi_tuple = (1, 2, 3, 4, 5)
 print(type(mi_tuple))
 print(mi_tuple)
+
+nueva_tupla = (45, 2.3, 'hola', True)
+print(nueva_tupla)
+print(nueva_tupla[1])
+
+x, y, z, u = nueva_tupla
+print(x)
+print(z)
+print(nueva_tupla.count(45))
+print(nueva_tupla.index(True))
