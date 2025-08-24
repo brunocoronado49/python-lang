c4316ba248c575aaf3ce4115d64c7f212fbd51a0
brunocoronado49 <francorangelcoronado@gmail.com>
2025-08-24 10:13:16 -0600

    Usabdi el modulo shutil para comprimir

--- 09-dia/zip_unzip.py
+++ 09-dia/zip_unzip.py
@@ -1,7 +1,12 @@
 import zipfile
+import shutil
 
 
 mi_zip = zipfile.ZipFile('archivo_comprimido_dia_9.zip', 'w')
 mi_zip.write('texto_a.txt')
 mi_zip.write('texto_b.txt')
 mi_zip.close()
+
+carpeta_origen = '/home/bruce117/Documents/Resume'
+archivo_destino = 'CV_COMPRIMIDO'
+shutil.make_archive(archivo_destino, 'zip', carpeta_origen)
